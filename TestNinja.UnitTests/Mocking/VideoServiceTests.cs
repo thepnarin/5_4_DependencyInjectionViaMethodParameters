3ead13afd65be61ab619dbffaf59afce83f8761e
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-20 14:08:10 +0700

    DependencyInjectionViaMethodParameters

---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _5_4_DependencyInjectionViaMethodParameters.Mocking;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    class VideoServiceTests
+    {
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
+            var service = new VideoService();
+
+            var result = service.ReadVideoTitle(new FakeFileReader());
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
---
+++ TestNinja.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestNinja.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4bc5d1b2-e85c-4e62-938a-089438336d42")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
